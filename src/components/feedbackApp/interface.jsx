3c9801e400b4ff9735cff217290b7535d7a8ceeb
Mateuszmeteo <mateusz.trepkowski@gmail.com>
2023-06-13 17:02:54 +0200

    first-step

--- src/components/feedbackApp/interface.jsx
+++ src/components/feedbackApp/interface.jsx
@@ -2,17 +2,43 @@ import React from "react"
 import { Component } from "react"
 
 class Interface extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            good: 0,
+            neutral: 0,
+            bad: 0
+          }
+    }
+
+
+    handleButtonClick = (type) => {
+        this.setState((prevState) => (
+            {[type]:prevState[type] + 1}
+        ))
+    }
+
+    // countTotalFeedback()
+
+    // countPositiveFeedbackPercentage()
+
+
+
+
     render() {
+        const { good, neutral, bad} = this.state
+        // const params = this.state 
+
         return(
         <div>
             <h3>Please leave feedback</h3>
-            <button>Good</button>
-            <button>Neutral</button>
-            <button>Bad</button>
+            <button onClick={() => this.handleButtonClick('good')}>Good</button>
+            <button onClick={() => this.handleButtonClick('neutral')}>Neutral</button>
+            <button onClick={() => this.handleButtonClick('bad')}>Bad</button>
             <h3>Statistic</h3>
-            <p>Good: 3</p>
-            <p>Neutral: 2</p>
-            <p>Bad: 2</p>
+            <p>Good: {good}</p>
+            <p>Neutral: {neutral}</p>
+            <p>Bad: {bad}</p>
         </div>
     )
     }
